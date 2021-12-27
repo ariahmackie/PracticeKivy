16528cb5a8e919b3d273d570d1662e5a4294a11f
ariah <ariahmackie@gmail.com>
2021-12-27 00:00:45 -0800

    added textboxes and buttons

--- kivy_for_rpg/login_screen/login.py
+++ kivy_for_rpg/login_screen/login.py
@@ -2,13 +2,15 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivymd.app import MDApp
+from kivy.core.window import Window
 
+Window.size = (400,800)
 
 class LoginScreen(FloatLayout):
     pass
 
 
-class LoginApp(MDApp):
+class LoginApp(App):
     def build(self):
         return LoginScreen()
 
