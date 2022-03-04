16c63dcc62b61fef037a023b049eaa90c0ba61b8
ariah <ariahmackie@gmail.com>
2022-03-03 17:36:55 -0800

    updates xp now

--- python_databases/rpg_database.py
+++ python_databases/rpg_database.py
@@ -5,76 +5,18 @@ connection =sqlite3.connect("rpg_data.db")
 cursor = connection.cursor()
 
 #Tuples
-# (level, coins, experience, strength, perception, intelligence, charisma)
+                                                                                                                            # (level, coins, experience, strength, perception, intelligence, charisma)
 
 #description, user, health_value
-fruit_tuples =[
-("apple", "any", 2),
-("banana", "any", 3),
-("berries", "any", 1),
-("cherries", "any", 2),
-("coconut", "any", 3),
-("grapefruit", "any", 4),
-("grapes", "any", 1),
-("kiwi", "any", 2),
-("lemon", "any", 3),
-("lime", "any", 4),
-("mango", "any", 1),
-("orange", "any", 2),
-("peach", "any", 3),
-("pear", "any", 4),
-("pineapple", "any", 1),
-("strawberries", "any", 2),
-("watermelon", "any", 3)]
+fruit_tuples =[("apple", "any", 2), ("banana", "any", 3), ("berries", "any", 1), ("cherries", "any", 2), ("coconut", "any", 3), ("grapefruit", "any", 4), ("grapes", "any", 1), ("kiwi", "any", 2), ("lemon", "any", 3), ("lime", "any", 4), ("mango", "any", 1), ("orange", "any", 2), ("peach", "any", 3), ("pear", "any", 4), ("pineapple", "any", 1),("strawberries", "any", 2), ("watermelon", "any", 3)]
 
-vegetable_tuples = [
-("artichoke", "any", 1),
-("asparagus", "any", 2),
-("broccoli", "any" , 3),
-("cabbage", "any", 4),
-("carrot", "any", 1),
-("cauliflower", "any", 2),
-("corn", "any", 3),
-("cucumber", "any", 4),
-("eggplant", "any", 1),
-("lettuce", "any", 2),
-("mushrooms", "any", 3),
-("onion", "any", 4),
-("peas", "any", 1),
-("potato", "any", 4),
-("radishes", "any", 2),
-("tomato", "any", 3)]
+vegetable_tuples = [("artichoke", "any", 1), ("asparagus", "any", 2), ("broccoli", "any" , 3), ("cabbage", "any", 4), ("carrot", "any", 1), ("cauliflower", "any", 2), ("corn", "any", 3), ("cucumber", "any", 4), ("eggplant", "any", 1), ("lettuce", "any", 2), ("mushrooms", "any", 3),("onion", "any", 4), ("peas", "any", 1), ("potato", "any", 4), ("radishes", "any", 2), ("tomato", "any", 3)]
 
-dessert_tuples = [
-("chocolate chip cookies", "any", 3),
-("apple pie", "any", 2),
-("cheese cake", "any", 1),
-("carrot cake", "any", 3),
-("icecream", "any", 2),
-("birthday cake", "any", 3),
-("cotton candy", "any", 2),
-("brownies", "any", 2),
-("pumpkin pie", "any", 3),
-("red velvet cake", "any", 2),
-("donut", "any", 2),
-("lollypop", "any", 1),
-("cherry pie", "any", 2)]
+dessert_tuples = [("chocolate chip cookies", "any", 3), ("apple pie", "any", 2), ("cheese cake", "any", 1), ("carrot cake", "any", 3), ("icecream", "any", 2), ("birthday cake", "any", 3), ("cotton candy", "any", 2), ("brownies", "any", 2), ("pumpkin pie", "any", 3), ("red velvet cake", "any", 2), ("donut", "any", 2), ("lollypop", "any", 1), ("cherry pie", "any", 2)]
 
-meal_tuples = [
-("ramen", "any", 1),
-("sushi", "any", 1),
-("chow mein", "any", 2),
-("orange chicken", "any", 4),
-("roast chicken", "any", 2),
-("rice", "any", 3),
-("taco", "any", 4),
-("roast beef", "any", 3),
-("baked potato", "any", 2),
-("enchilada", "any", 3),
-("sandwich", "any", 2)
-]
+meal_tuples = [("ramen", "any", 1), ("sushi", "any", 1), ("chow mein", "any", 2), ("orange chicken", "any", 4), ("roast chicken", "any", 2), ("rice", "any", 3), ("taco", "any", 4), ("roast beef", "any", 3), ("baked potato", "any", 2), ("enchilada", "any", 3), ("sandwich", "any", 2)]
 
-# PLAYER TABLE------------------------------------------------------------------
+#--------------------- PLAYER TABLE------------------------------------------------------------------
 def create_player_table():
     cursor.execute('''CREATE TABLE Player(
     id INTEGER PRIMARY KEY,
@@ -97,9 +39,8 @@ def add_new_player(email, username, password):
     cursor.execute(command, player_tuple)
 
 def get_xp(player_id):
-    player_id = (player_id,)
-    command = "SELECT experience WHERE id=?"
-    cursor.execute(command, player_id)
+    command = "SELECT experience FROM Player WHERE id=?"
+    cursor.execute(command, (player_id,))
     experience = cursor.fetchone()[0]
     return experience
 
@@ -109,7 +50,6 @@ def increase_xp(player_id, value):
     xp_tuple = (experience, player_id)
     command = "UPDATE PLAYER SET experience=? WHERE id=?"
     cursor.execute(command, xp_tuple)
-
 
 def drop_player_table():
     cursor.execute('DROP TABLE IF EXISTS Player')
@@ -123,7 +63,7 @@ def print_player_table():
         print(i)
     print("")
 
-#INVENTORY TABLE ---------------------------------------------------------------
+#-----------------------------INVENTORY TABLE ---------------------------------------------------------------
 def create_inventory_table():
     cursor.execute('''CREATE TABLE Inventory(
     id INTEGER NOT NULL PRIMARY KEY,
@@ -133,9 +73,8 @@ def create_inventory_table():
 def drop_inventory_table():
     cursor.execute('DROP TABLE IF EXISTS Inventory')
 
-def populate_inventory_table():
-    cursor.execute()
-#ITEM TABLE--------------------------------------------------------------------
+
+#-------------------------------ITEM TABLE--------------------------------------------------------------------
 def create_item_table():
     cursor.execute('''CREATE TABLE Item(
     id INTEGER NOT NULL PRIMARY KEY,
@@ -148,13 +87,10 @@ def populate_item_table():
     command = "INSERT INTO Item (name, user, value) VALUES (?, ?, ?)"
     for i in fruit_tuples:
         cursor.execute(command, i)
-
     for i in vegetable_tuples:
         cursor.execute(command, i)
-
     for i in dessert_tuples:
         cursor.execute(command, i)
-
     for i in meal_tuples:
         cursor.execute(command, i)
 
@@ -167,7 +103,7 @@ def print_item_table():
 def drop_item_table():
     cursor.execute('DROP TABLE IF EXISTS Item')
 
-# TASK TABLE ---------------------------------------
+#--------------------------------TASK TABLE ---------------------------------------
 def create_task_table():
     cursor.execute('''CREATE TABLE Tasks(
     id INTEGER PRIMARY KEY,
@@ -175,42 +111,44 @@ def create_task_table():
     duedate TEXT,
     value INTEGER NOT NULL,
     is_repeatable INTEGER NOT NULL,
-    person_id INTEGER NOT NULL,
+    player_id INTEGER NOT NULL,
     complete INTEGER NOT NULL,
-    FOREIGN KEY(person_id) REFERENCES Player(id));''')
+    FOREIGN KEY(player_id) REFERENCES Player(id));''')
 
 def drop_task_table():
     cursor.execute('DROP TABLE IF EXISTS Tasks')
 
 def create_task(description, duedate, value, is_repeatable, person_id):
-    command = "INSERT INTO Tasks (description, duedate, value, is_repeatable, person_id, complete) VALUES (?, ?, ?, ?, ?, ?)"
+    command = "INSERT INTO Tasks (description, duedate, value, is_repeatable, player_id, complete) VALUES (?, ?, ?, ?, ?, ?)"
     task_tuple = (description, duedate, value, is_repeatable, person_id, 0)
     cursor.execute(command, task_tuple)
 
 def delete_task(task_id):
-    task_id = (task_id,)
     command = "DELETE FROM Tasks WHERE id=?"
-    cursor.execute(command, task_id)
+    cursor.execute(command, (task_id,))
 
 def complete_task(task_id):
-    task_id = (task_id,)
     command = "UPDATE Tasks SET complete = 1 WHERE id=?"
-    cursor.execute(command, task_id)
-    cursor.execute("SELECT value FROM Tasks WHERE id=?", task_id)
+    cursor.execute(command, (task_id,))
+    cursor.execute("SELECT value FROM Tasks WHERE id=?", (task_id,))
     value = cursor.fetchone()[0]
+    player_id = get_playerid_from_taskid(task_id)
+    increase_xp(player_id, value)
 
-def get_player_id_from_task(task_id):
-    pass
-
+def get_playerid_from_taskid(task_id):
+    cursor.execute("SELECT player_id FROM Tasks WHERE id=?", (task_id,))
+    player_id = cursor.fetchone()[0]
+    return player_id
 
 def print_task_table():
-    task_tuple = ("id", "description", "duedate", "value", "is_repeatable", "person_id", "complete")
+    task_tuple = ("id", "description", "duedate", "value", "is_repeatable", "player_id", "complete")
     print(task_tuple)
     data = cursor.execute("SELECT * FROM Tasks")
     for i in data:
         print(i)
     print('')
 
+#------------------TEST FUNCTIONS -----------------------
 drop_player_table()
 drop_inventory_table()
 drop_task_table()
@@ -226,3 +164,5 @@ create_task("walk the dog", "3-4-2021", 10, 0, 1)
 print("before completing task")
 print_player_table()
 complete_task(1)
+print("after completing task")
+print_player_table()
