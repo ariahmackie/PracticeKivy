29cbb7caa4fb5c8995aa49ae7a73e64266e4ecb1
ariah <ariahmackie@gmail.com>
2022-06-07 23:05:19 -0700

    multiple_files_screenmanager

--- multiple_files_screenmanager/main.py
+++ multiple_files_screenmanager/main.py
@@ -4,18 +4,27 @@ import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file("login.kv")
-Builder.load_file("home.kv")
-Builder.load_file("newtask.kv")
-
-class main_kv(GridLayout):
+class LoginWindow(Screen):
 	pass
+
+class HomeWindow(Screen):
+	pass
+
+class NewTaskWindow(Screen):
+	pass 
+
+class WindowManager(ScreenManager):
+	pass
+
+main = Builder.load_file("main.kv")
+home = Builder.load_file("home.kv")
+newtask = Builder.load_file("newtask.kv")
 
 class MainApp(App):
 	def build(self):
-		self.x = 150
-		self.y = 400
-		return main_kv()
+		return main
+
 if __name__ == '__main__':
 	MainApp().run()
